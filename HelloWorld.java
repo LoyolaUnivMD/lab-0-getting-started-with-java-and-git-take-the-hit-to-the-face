2d7f6d08b21ded1f9502605d471b0966100609c9
Gigi <gli@loyola.edu>
2024-01-19 20:28:20 -0500

    Created my initials and added new comments since I don't think we have lab partners for lab 0. Even if we did, I forgot...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,12 @@
 //HelloWorld.java
+// Programmers: Gigi Li
+// Course:  CS 212, Mr. John
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: None
 
 class HelloWorld {
     public static void main(String[] args) {
