022ed309042597e8cab74a81dac2d6b7f95ee65f
Gigi <gli@loyola.edu>
2024-01-19 20:33:01 -0500

    Created my initials and added new comments since I don't think we have lab partners for lab 0. Even if we did, I forgot... Created a snowman to celebrate the winter :)

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,12 +6,20 @@
 // Problem Statement:  Output programmers’ initial
 // Input: None; there is no user input in this program
 // Output:  The initials of the programmers in ASCII art form
-// Credits: None
+// Credits: ASCII Art Archive
 
 class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
         System.out.println("  ____ _     \n / ___| |    \n| |  _| |    \n| |_| | |___ \n \\____|_____|");
+        System.out.println(".      *    *           *.       *   .                      *     .");
+        System.out.println("               .   .                   __   *    .     * .     *");
+        System.out.println("    *       *         *   .     .    _|__|_        *    __   .       *");
+        System.out.println("  .  *  /\\       /\\          *        ('')    *       _|__|_     .");
+        System.out.println("       /  \\   * /  \\  *          .  <( . )> *  .       ('')   *   *");
+        System.out.println("  *    /  \\     /  \\   .   *       _(__.__)_  _   ,--<(  . )>  .    .");
+        System.out.println("      /    \\   /    \\          *   |       |  )),`   (   .  )     *");
+        System.out.println("   *   `||` ..  `||`   . *.   ... ==========='`   ... '--`-` ... * jb .");
     }
 }
