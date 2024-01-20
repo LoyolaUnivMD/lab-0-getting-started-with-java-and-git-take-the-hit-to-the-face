fb467231015009f3b8f7199ec8659ceca4e7ddb2
Gigi <gli@loyola.edu>
2024-01-19 20:24:47 -0500

    Created my initials since I don't think we have lab partners for lab 0. Even if we did, I forgot...

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        
+        System.out.println("  ____ _     \n / ___| |    \n| |  _| |    \n| |_| | |___ \n \\____|_____|");
     }
 }
